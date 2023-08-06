aff7977817ed092cc897956abbe96be80ee7146b
Santiago Vizcaino <jvizcainot@est.ups.edu.ec>
2023-08-06 15:49:33 -0500

    [Add]: commit inicial

---
+++ src/app/heroes/hero/hero.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-heroes-hero',
+  templateUrl: './hero.component.html',
+  styleUrls: ['./hero.component.css']
+})
+export class HeroComponent {
+  public name:string="iroman"
+  public age:number=45
+
+  get capitalizeName():string{
+    return this.name.toUpperCase();
+  }
+
+  getHeroDescription():string{
+    return `${this.name} - ${this.age}`
+  }
+
+  changeHeroe():void{
+    this.name='osito'
+  }
+  changeAge():void{
+    this.age=35
+  }
+  resetForm():void{
+    this.name="iroman"
+    this.age=45
+  }
+}
